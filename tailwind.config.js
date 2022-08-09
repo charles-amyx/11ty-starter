ffd5c5ccd2110a27dd9a81141f91ec15eaf1c747
Charles E.Amyx, III <ceamyx@gmail.com>
2022-08-09 13:58:50 -0400

    fix: temp fix accordion height issue
    Going to have to revisit this as it is less than ideal

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: {
       colors,
       fontFamily: {
-        sans: ['Mulish', ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
       },
       spacing,
     },
