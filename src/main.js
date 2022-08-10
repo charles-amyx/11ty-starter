d7aa10318c93768c9a0fe3725c1fcf9b6d454d85
Charles E.Amyx, III <ceamyx@gmail.com>
2022-08-10 16:24:59 -0400

    chore: add focus plugin for alpine

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import './styles/main.css';
 
 import Alpine from 'alpinejs';
 import intersect from '@alpinejs/intersect';
+import focus from '@alpinejs/focus';
 
 window.Alpine = Alpine;
 
+Alpine.plugin(focus);
 Alpine.plugin(intersect);
 Alpine.start();
 
